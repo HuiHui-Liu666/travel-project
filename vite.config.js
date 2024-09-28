2c2ec75406380a24908148eb3b49265afe488d93
Hanyu <191077050@qq.com>
2024-09-28 17:27:55 +0800

    feat：配置@别名、手机调试

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { resolve } from 'path'
+const pathResolve = (p) => resolve(__dirname, '.', p)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server:{
+    host:'192.168.31.55',
+    port:8080
+  },
+  resolve:{
+    alias:{
+      '@':[{find:'@', replacement:pathResolve('src')}]
+    }
+  }
 })
