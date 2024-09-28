5094daafc0c07a1b341768033a35f68a8c9cfee9
Hanyu <191077050@qq.com>
2024-09-28 17:17:39 +0800

    feat: vant 的使用 、rem适配
    【特别注意官网说的创建.postcss.config.js文件  要报错 需要创建.cjs】

---
+++ postcss.config.cjs
@@ -0,0 +1,13 @@
+// postcss.config.js
+module.exports = {
+  plugins: {
+    // postcss-pxtorem 插件的版本需要 >= 5.0.0
+    'postcss-pxtorem': {
+      // 如果设计稿的尺寸不是 375，而是 750 或其他大小，可以如下配置
+      rootValue({ file }) {
+        return file.indexOf('vant') !== -1 ? 37.5 : 75;
+      },
+      propList: ['*'],
+    },
+  },
+};
--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
 <template>
   <div>
     <h2>hello world!</h2>
+    <van-button>jjj</van-button>
+    <van-button type="primary">主要按钮</van-button>
+<van-button type="success">成功按钮</van-button>
   </div>
 </template>
 
 <style scoped></style>
+ 
