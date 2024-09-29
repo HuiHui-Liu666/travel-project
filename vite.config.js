ac87312874bb8d3c6eae3ce7eeafa4a209a732c0
Hanyu <191077050@qq.com>
2024-09-29 11:06:59 +0800

    feat: 修改配置别名

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import { resolve } from 'path'
-const pathResolve = (p) => resolve(__dirname, '.', p)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -12,7 +11,7 @@ export default defineConfig({
   },
   resolve:{
     alias:{
-      '@':[{find:'@', replacement:pathResolve('src')}]
+      '@':resolve(__dirname,'src')
     }
   }
 })
